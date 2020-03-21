7fcf9aa0c1f4627ec0214bbaefaaa6f627d703d8
alecweems <60677887+alecweems@users.noreply.github.com>
2020-03-21 19:35:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
+
+
+// got an API key from the openweather website
+var apiKey = "18d00ed37aadb5dd1d52f88f6b2cd497"
